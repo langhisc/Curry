5484b91e9ad9f3f67b83449a2700b4c768b41b19
Sean Langhi <langhisc@gmail.com>
2022-12-22 13:41:39 -0800

    Make all functions public, as originally intended

--- Sources/Curry/Curry.swift
+++ Sources/Curry/Curry.swift
@@ -17,21 +17,21 @@ infix operator ^^ : CurryingPrecedence
 
 // Takes a ternary function.  Returns a "closure factory".
 // The factory accepts a constant argument, and returns a binary closure that will run the original ternary function with the constant argument in position 1, and the two closure parameters in positions 2 and 3.
-func curry<A, B, C, D>(_ ternaryFunc: @escaping (A, B, C) -> D )
+public func curry<A, B, C, D>(_ ternaryFunc: @escaping (A, B, C) -> D )
 ->
 (A) -> ((B, C) -> D) {
     return { a in { b, c in ternaryFunc(a, b, c) } }
 }
 
 // Fixes the first argument of a ternary function, yielding a binary function.
-func ^^ <A, B, C, D>(ternaryFunc: @escaping (A, B, C) -> D, arg: A)
+public func ^^ <A, B, C, D>(ternaryFunc: @escaping (A, B, C) -> D, arg: A)
 ->
 (B, C) -> D {
     curry(ternaryFunc)(arg)
 }
 
 // Fixes all arguments of a ternary function, yielding a zero-arity function.
-func ^^ <A, B, C, D>(ternaryFunc: @escaping (A, B, C) -> D, args: (A, B, C))
+public func ^^ <A, B, C, D>(ternaryFunc: @escaping (A, B, C) -> D, args: (A, B, C))
 ->
 () -> D {
     return { ternaryFunc(args.0, args.1, args.2) }
@@ -39,21 +39,21 @@ func ^^ <A, B, C, D>(ternaryFunc: @escaping (A, B, C) -> D, args: (A, B, C))
 
 // Takes a binary function.  Returns a "closure factory".
 // The factory accepts a constant LHS, and returns a unary closure that will run the original binary function with the constant LHS and a parameterized RHS.
-func curry<A, B, C>(_ binaryFunc: @escaping (A, B) -> C )
+public func curry<A, B, C>(_ binaryFunc: @escaping (A, B) -> C )
 ->
 (A) -> ((B) -> C) {
     return { a in { b in binaryFunc(a, b) } }
 }
 
 // Fixes the first argument of a binary function, yielding a unary function.
-func ^^ <A, B, C>(binaryFunc: @escaping (A, B) -> C, arg: A)
+public func ^^ <A, B, C>(binaryFunc: @escaping (A, B) -> C, arg: A)
 ->
 (B) -> C {
     curry(binaryFunc)(arg)
 }
 
 // Fixes both arguments of a binary function, yielding a zero-arity function.
-func ^^ <A, B, C>(binaryFunc: @escaping (A, B) -> C, args: (A, B))
+public func ^^ <A, B, C>(binaryFunc: @escaping (A, B) -> C, args: (A, B))
 ->
 () -> C {
     return { binaryFunc(args.0, args.1) }
@@ -61,14 +61,14 @@ func ^^ <A, B, C>(binaryFunc: @escaping (A, B) -> C, args: (A, B))
 
 // Takes a unary function.  Returns a "closure factory".
 // The factory accepts a constant argument, and returns a zero-arity closure that will run the original unary function with the constant argument.
-func curry<A, B>(_ unaryFunc: @escaping (A) -> B )
+public func curry<A, B>(_ unaryFunc: @escaping (A) -> B )
 ->
 (A) -> (() -> B) {
     return { a in { unaryFunc(a) } }
 }
 
 // Fixes the argument of a unary function, yielding a zero-arity function.
-func ^^ <A, B>(_ unaryFunc: @escaping (A) -> B, arg: A)
+public func ^^ <A, B>(_ unaryFunc: @escaping (A) -> B, arg: A)
 ->
 () -> B {
     return curry(unaryFunc)(arg)
